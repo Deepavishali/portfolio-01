9e949d5ebe7b8636781aecb1a93333f6fce7d72c
Deepavishali <deepavishalig@gmail.com>
2023-07-24 21:38:21 +0530

    added projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,8 @@
 
 import crm from '../assets/svg/projects/crm.gif'
 import mba from '../assets/svg/projects/mba.png'
+import chat from '../assets/svg/projects/chat.png'
+import library from '../assets/svg/projects/library.png'
 
 export const projectsData = [
     {
@@ -20,6 +22,25 @@ export const projectsData = [
         code: 'https://github.com/Deepavishali/movie-frontend-app',
         demo: 'https://web-2-movie-booking.netlify.app/',
         image: mba
+    }
+    ,
+     {
+        id: 3,
+        projectName: 'Chat Application',
+         projectDesc: 'This project enables users to search and chat with others, fostering seamless communication and collaboration',
+        tags: ['React.js', 'Express.js', 'MongoDB', 'Node.js'],
+         code: 'https://github.com/Deepavishali/chat-application',
+         demo: 'https://deepavishali-3rd-pro-chat-app.netlify.app/',
+        image: chat
+    },
+    {
+        id: 4,
+        projectName: 'Library Management',
+        projectDesc: 'This project serves as a concise demo of a library management system',
+        tags: ['React.js', 'Express.js', 'MongoDB', 'Node.js'],
+        code: 'https://github.com/Deepavishali/day32.git',
+        demo: 'https://funny-caramel-8bbbd8.netlify.app/',
+        image: library
     }
     ]
 
